9363632459e1d0ee52b655ca3225a776da289b51
Joel Ye <joelye9@gmail.com>
2018-10-05 13:37:32 -0400

    make baseline, add float to bool primitives so we get a working baseline!

--- primitives.py
+++ primitives.py
@@ -1,3 +1,15 @@
 # Some place for primitives
+
+# [bool, float, float] -> float
 def if_then_else(input, output1, output2):
     return output1 if input else output2
+
+# [float, float] -> bool
+def is_greater(input1, input2):
+    return input1 > input2
+
+def is_equal_to(input1, input2):
+    return input1 == input2
+
+def relu(input1):
+    return input1 if input1 > 0 else 0
